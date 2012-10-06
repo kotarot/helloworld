b9fbd7d94922acbafc2f8f7876f15836a8da770c
Kotaro Terada <tera@toki.waseda.jp>
2012-10-07 03:54:07 +0900

    create java

---
+++ Helloworld.java
@@ -0,0 +1,13 @@
+/*
+ * Filename: Helloworld.java
+ * Language: Java
+ * Compiler: 
+ * javac 1.6.0_24
+ */
+
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java world!");
+    }
+}
+
