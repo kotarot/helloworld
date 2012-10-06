54924a86bd688199c521c20ff9fe8e8c762bd1f1
Kotaro Terada <tera@toki.waseda.jp>
2012-10-07 04:26:21 +0900

    create D

---
+++ helloworld.d
@@ -0,0 +1,15 @@
+#!/usr/bin/dmd -run
+
+/*
+ * Filename: helloworld.d
+ * Language: D
+ * Compiler: DMD32 D Compiler v2.060
+ */
+
+import std.stdio;
+
+int main() {
+    writeln("Hello, D world!");
+    return 0;
+}
+
