86b0820be6b301f90a89de2d33403b6bf1c66a29
Kotaro Terada <tera@toki.waseda.jp>
2012-10-07 04:38:31 +0900

    create fortran

---
+++ helloworld.f90
@@ -0,0 +1,9 @@
+! Filename: helloworld.f90
+! Language: Fortran90
+! Compiler: GNU Fortran (Ubuntu/Linaro 4.6.3-1ubuntu5) 4.6.3
+
+PROGRAM HELLOWORLD
+    WRITE(*,*) 'Hello, Fortran world!'
+    STOP
+END PROGRAM HELLOWORLD
+
