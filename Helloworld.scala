c1b8f1eb2dcd3266507c2a489738f09c424f6adb
Kotaro Terada <tera@toki.waseda.jp>
2012-10-07 04:46:38 +0900

    create Scala

---
+++ Helloworld.scala
@@ -0,0 +1,12 @@
+/*
+ * Filename: Helloworld.scala
+ * Language: Scala
+ * Compiler: Scala compiler version 2.9.1
+ */
+
+object Helloworld {
+    def main(arg: Array[String]) = {
+        println("Hello, Scala world!")
+    }
+}
+
