9fcddfcce3c448f75b1f323e27ef2b9b7aaec0ba
Kotaro Terada <tera@toki.waseda.jp>
2012-10-07 18:41:02 +0900

    create Perl

---
+++ helloworld.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+
+# Filename: helloworld.pl
+# Language: Perl
+# Interpreter: perl 5, version 14, subversion 2 (v5.14.2) built for i686-linux-gnu-thread-multi-64int
+
+print "Hello, Perl world!\n";
+
