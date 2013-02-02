c9d74a79830a7fa77a471b559bf8cfe5de0348e0
Kotaro Terada <cq.cq.de.arjgk@gmail.com>
2013-02-02 19:39:50 +0900

    COBOL作った

---
+++ helloworld.cob
@@ -0,0 +1,9 @@
+      *Filename: helloworld.cob
+      *Language: COBOL
+      *Compiler: cobc (OpenCOBOL) 1.0.0
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLOWORLD.
+       PROCEDURE DIVISION.
+       DISPLAY "Hello, COBOL World!".
+       STOP RUN.
+
